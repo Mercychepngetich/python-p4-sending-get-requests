df6fdd3725c91569b66975e98cbaf9ff3a039cdd
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-10-18 07:19:51 +0300

    getting data done

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,18 @@
 import requests
 import json
 
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+url = "https://learn-co-curriculum.github.io/json-site-example/"
 
 response = requests.get(url)
 
-json_content = json.loads(response.text)
 
-print(json.dumps(json_content, indent=4))
+
+# print(json.dumps(json_content, indent=4))
+# print(response.text)
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+
+response = requests.get(url)
+json_content= json.loads(response.content)
+
+print(json.dumps(json_content, indent=4, sort_keys=True))
